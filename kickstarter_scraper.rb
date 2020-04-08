40b8374443994ef9f6e101e3968fab1f45d8d494
Justin Winger <justinwinger4@gmail.com>
2020-04-08 21:37:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,10 @@
-require 'nokogiri'
-require 'pry'
-
+require "nokogiri"
+require "pry"
+ 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
+ 
+create_project_hash
